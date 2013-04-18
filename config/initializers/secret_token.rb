eab7fceb3fae5b481912772144396e38966fe949
naush <li-hsuan@namely.com>
2013-04-18 19:26:46 +0800

    Welcome page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sleepphones::Application.config.secret_token = '738be25af03e3586a8ef7301ad69d3c51a65ea171e73d433970b902423bd590b2c14e5a30e86f78549d521478cbbe05481bbbc5e4cb2ef5f6c89bc3d13f21582'
