f1ee43820ba85c3da86260e3f7723e5f53a7e250
naush <li-hsuan@namely.com>
2013-04-20 01:47:10 +0800

    Flip media coverage quotes

---
+++ app/assets/javascripts/sleepphones.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.flip').quickFlip();
+
+  var flip = function() {
+    var random = Math.round(Math.random() * $('.flip').size());
+    var target = $($('.flip')[random]);
+    target.quickFlipper({
+      refresh: true
+    });
+  };
+
+  setInterval(flip, 3000);
+});
